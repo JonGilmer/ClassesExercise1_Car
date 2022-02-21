f4a11249add903cf6dc8005fbd07155e8470a284
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-02-20 21:34:18 -0500

    Initial commit of Classes Exercise 1 to remote Repo

---
+++ ClassesExercise1_Car/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassesExercise1_Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
