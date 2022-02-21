35b108a3e5bf1cc5d46ef4b64ced92a751478096
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-02-20 22:28:05 -0500

    Completed version of Classes Exercise 1 - Cars

---
+++ ClassesExercise1_Car/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassesExercise1_Car
+{
+    public class Car
+    {
+        public Car() // Default Constructor
+        {
+
+        }
+
+        // Parameterized Constructor
+        public Car(string make, string model, int year )
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+
+        public string Make { get; set; } // Public "Make" property of type string
+
+        public string Model { get; set; } // Public "Model" property of type string
+
+        public int Year { get; set; } // Public "Year" property of type int
+    }
+}
--- ClassesExercise1_Car/Program.cs
+++ ClassesExercise1_Car/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ClassesExercise1_Car
 {
@@ -6,7 +7,46 @@ namespace ClassesExercise1_Car
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car car1 = new Car()
+            {
+                Make = "Ferrari",
+                Model = "F50",
+                Year = 1995
+            };
+
+
+            var car2 = new Car();
+            car2.Make = "Koenigsegg";
+            car2.Model = "CCR";
+            car2.Year = 2006;
+
+
+            var car3 = new Car("Saleen", "S7", 2003);
+
+
+            PrintCars(car1, car2, car3);
+
+
+
+
         }
+
+        public static void PrintCars(Car carX, Car carY, Car carZ)
+        {
+            List<Car> carList = new List<Car>()
+            {
+                carX,
+                carY,
+                carZ
+            };
+
+            foreach (var car in carList)
+            {
+                Console.WriteLine($"{car.Make} \n {car.Model} \n {car.Year}");
+            }
+
+        }
+
+
     }
 }
